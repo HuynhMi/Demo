58cc08300b327484ec8e4ecc2a0f1d0209c4b156
HuynhMi <54268240+HuynhMi@users.noreply.github.com>
2021-10-30 14:52:32 +0700

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then((reg) => {
+        console.log('SW has registered!',reg);
+    })
+    .catch((err) => {
+        console.log('SW has not registered!',err);
+    })
+}
